7030eea1e06fc88a65a288a5b8df91e513217e29
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-04-17 07:49:43 +0100

    fixed bug

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -11,7 +11,7 @@ export const SideBar = ({ setPublishPost }: { setPublishPost: React.Dispatch<Rea
     return     <div className="md:col-span-2  bg-white shadow-2xl fixed z-[100] gap-2 top-[120px] w-[300px]  w-[30%]  md:right-[50px] right-[20px] rounded-[20px] p-2 py-[20px] hidden md:flex flex-col justify-center">
    {loggedInUser ? <ul className="flex flex-col gap-3 items-center ">
       <div className="flex flex-col border  rounded-[10px] py-[10px] px-[20px] items-center">
-     {loggedInUser.photoURL?  <Image className="rounded-full" alt={loggedInUser?.displayName} src={loggedInUser?.photoURL} height={50} width={50} /> : <FaUserCircle className="text-[70px] bg-slate-50 rounded-full  " />}
+     {loggedInUser?.photoURL?  <Image className="rounded-full" alt={`${loggedInUser?.displayName} profile pic`} src={loggedInUser?.photoURL} height={50} width={50} /> : <FaUserCircle className="text-[70px] bg-slate-50 rounded-full  " />}
           <h1 className="text-[20px] font-bold">{loggedInUser.displayName}</h1>
         <Link className="bg-sky-500  w-full text-center text-slate-50 rounded p-1" href='/my-profile'>Full Profile</Link>
       </div>
