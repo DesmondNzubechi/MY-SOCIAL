3eca9eba735e53f946156fd077aac1ff434b6057
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-04-17 07:45:40 +0100

    fixed bug

--- app/components/protected  route/protected.tsx
+++ app/components/protected  route/protected.tsx
@@ -1,9 +1,6 @@
 import { userAuth } from "../auths/auth";
 import Login from "@/app/login/page";
 
-interface ProtectedRouteProps {
-    children: ReactNode;
-}
 
 export const ProtectedRoute = () => {
     const user = userAuth();
