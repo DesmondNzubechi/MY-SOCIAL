a991f91adb2884b0c1c6844422e772041fac1cae
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-04-18 05:20:55 +0100

    new change

--- app/components/sidebar/sidebar.tsx
+++ app/components/sidebar/sidebar.tsx
@@ -1,18 +1,44 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
 import { IoMdLogOut } from "react-icons/io";
 import { userAuth } from "../auths/auth";
 import Image from "next/image";
-
+import { AllUser } from "../allUser/allUser";
+import { personalInfo } from "@/app/my-profile/page";
 export const SideBar = ({ setPublishPost }: { setPublishPost: React.Dispatch<React.SetStateAction<string>> }) => {
+  
+  const allUser = AllUser()
   const loggedInUser = userAuth();
+  const [userPersonalInfo, setUserPersonalInfo] = useState<personalInfo>({
+    userID: "",
+    fullname: "",
+    useremail: "",
+    userPic:"",
+    coverPic: "",
+    username: "",
+    bio: "",
+    location: "",
+    favorite: "",
+    dateJoined: ''
+  })
+
+  const getUserProfile = () => {
+    const findUser = allUser.find((profile: any) => {
+      return profile.userID === loggedInUser?.uid
+    })
+    setUserPersonalInfo(findUser);
+  }
+
+  useEffect(() => {
+    getUserProfile()
+  }, [allUser, loggedInUser])
     return     <div className="md:col-span-2  bg-white shadow-2xl fixed z-[100] gap-2 top-[120px] w-[300px]  w-[30%]  md:right-[50px] right-[20px] rounded-[20px] p-2 py-[20px] hidden md:flex flex-col justify-center">
    {loggedInUser ? <ul className="flex flex-col gap-3 items-center ">
       <div className="flex flex-col border  rounded-[10px] py-[10px] px-[20px] items-center">
-     {loggedInUser?.photoURL?  <Image className="rounded-full" alt={`${loggedInUser?.displayName} profile pic`} src={loggedInUser?.photoURL} height={50} width={50} /> : <FaUserCircle className="text-[70px] bg-slate-50 rounded-full  " />}
-          <h1 className="text-[20px] font-bold">{loggedInUser.displayName}</h1>
+     {userPersonalInfo?.userPic?  <Image className="rounded-full" alt={`${userPersonalInfo?.username} profile pic`} src={userPersonalInfo?.userPic} height={50} width={50} /> : <FaUserCircle className="text-[70px] bg-slate-50 rounded-full  " />}
+          <h1 className="text-[20px] font-bold">{userPersonalInfo?.username}</h1>
         <Link className="bg-sky-500  w-full text-center text-slate-50 rounded p-1" href='/my-profile'>Full Profile</Link>
       </div>
     <Link href='/' className="text-slate-700 text-[20px] capitalize ">Home</Link>
