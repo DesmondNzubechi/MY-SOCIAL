9728ec6240fe65b7f29644dd9b7f5ca71d4a6d86
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-04-24 04:26:26 +0100

    new change

--- app/components/full post/fullPost.tsx
+++ app/components/full post/fullPost.tsx
@@ -143,7 +143,7 @@ export const FullPost = ({ setShowFullPost, data, postComment, setFullPostData }
         <div>
         <div className={`fixed  flex flex-col justify-center items-center top-0 bottom-0 left-0 right-0 h-full w-full bg-Tp z-[1000]`}>
         <div className="bg-slate-50 relative overflow-y-auto md:w-[700px] w-full md:h-[90vh] p-4   ">
-        <h1 onClick={() => setShowFullPost(false)} className="uppercase text-white fixed z-[10] right-[35px] top-[10px] bg-slate-900 focus:bg-slate-500 cursor-pointer text-[20px] px-[12px] rounded-full py-[4px] ">X</h1>
+        <h1 onClick={() => setShowFullPost(false)} className="uppercase text-white fixed z-[10] right-[35px] md:right-[450px] md:top-[50px] top-[10px] bg-slate-900 focus:bg-slate-500 cursor-pointer text-[20px] px-[12px] rounded-full py-[4px] ">X</h1>
           <div className=" p-2 gap-[20px] relative pb-[70px] flex-col flex">
             <div className="flex gap-1 flex-row items-center">
             <h1 className="font-bold flex  capitalize items-center ">  {data.authorPics !== '' ? <Image src={data.authorPics} height={50} width={50} className="rounded-full w-[30px] md:w-[50px] " alt="post pic" /> :  <FaUserCircle className="md:text-[30px] text-[20px] bg-slate-50 rounded-full shadow-2xl " />} <span className="md:text-[15px] text-[10px] ">@{data.authorName}</span> </h1> <span className="text-slate-500 text-[8px] md:text-[12px] ">posted this</span> <GoDotFill className="text-[10px] "/> <p className="text-slate-500 text-[8px] md:text-[12px]">{data.postsDate}</p>
@@ -154,9 +154,9 @@ export const FullPost = ({ setShowFullPost, data, postComment, setFullPostData }
             </div>
             <Image src={data.postImg} height={400} width={500} className="w-full  " alt="post pic" />
             <div className="flex items-center border-t border-b bg-slate-50 py-[10px] justify-around">
-              <div className=" flex items-center p-[5px] gap-x-[5px] rounded"><FaCommentAlt className="text-[20px] "/> <p className="text-slate-500">{data.postComment.length} Comments</p></div>
-              <div onClick={() => likePost(data)} className="cursor-pointer flex items-center p-[5px] gap-x-[5px] rounded"><SlLike className="text-[20px] "/> <p className="text-slate-500">{data.postLike.length} Likes</p></div>
-              <div className=" flex items-center p-[5px] gap-x-[5px] rounded"><BiRepost className="text-[20px] " /><p className="text-slate-500">{data.postRepost} Repost</p></div>
+              <div className=" flex items-center p-[5px] gap-x-[5px] rounded"><FaCommentAlt className="md:text-[20px] text-[12px] "/> <p className="text-slate-500 text-[10px] md:text-[15px]">{data.postComment.length} Comments</p></div>
+              <div onClick={() => likePost(data)} className="cursor-pointer flex items-center p-[5px] gap-x-[5px] rounded"><SlLike className="md:text-[20px] text-[12px] "/> <p className="text-slate-500 text-[10px] md:text-[15px]">{data.postLike.length} Likes</p></div>
+              <div className=" flex items-center p-[5px] gap-x-[5px] rounded"><BiRepost className="md:text-[20px] text-[12px]" /><p className="text-slate-500 text-[10px] md:text-[15px]">{data.postRepost} Repost</p></div>
                         </div>
                         {/* COMMENTS UNDER POST */}
                         <div className="flex flex-col gap-5">
@@ -166,7 +166,7 @@ export const FullPost = ({ setShowFullPost, data, postComment, setFullPostData }
                     const { commenterName, commentDate, commentContent, commenterPic } = comment;
                     console.log("comments data", comment)
                     return  <div key={index} className="flex flex-start gap-1">
-                    <FaUserCircle className="md:text-[30px] text-[15px] bg-slate-50 rounded-full shadow-2xl " />
+                   {commenterPic? <Image src={commenterPic} height={50} width={50} className="rounded-full h-[30px] md:h-[50px] w-[30px] md:w-[50px] " alt="post pic" /> :  <FaUserCircle className="md:text-[30px] text-[15px] bg-slate-50 rounded-full shadow-2xl " />}
                     <div className='flex flex-col gap-1 bg-slate-200 rounded-bl-[20px]  rounded-r-[20px] p-3'>
                 <div className="flex gap-1 flex-row items-center">
                           <h1 className="font-bold text-[10px] md:text-[15px] flex items-center "> @{commenterName}</h1> <span className="text-slate-500 text-[8px] md:text-[10px] ">Commented</span> <GoDotFill className="text-[10px]"/> <p className="text-slate-500 text-[8px] md:text-[10px]">{commentDate}</p>
@@ -180,10 +180,10 @@ export const FullPost = ({ setShowFullPost, data, postComment, setFullPostData }
        
                         </div>
                         {/* ADD YOUR COMMENTS */}
-                        <div className="bg-slate-50 bottom-0 md:left-[24%] md:right-[24%] fixed left-0 right-0 border-t md:bottom-[20px] p-2">
+                        <div className="bg-slate-50 bottom-0 md:left-[26.9%] md:right-[26.9%] fixed left-0 right-0 border-t md:bottom-[20px] p-2">
                         <div className="py-[10px] w-full bg-white flex items-center justify-around px-[20px] gap-1">
-                <input type="text" onChange={(e) => setCommentInput(e.target.value)} placeholder="Input your comment" value={commentInput} className="w-full outline-none bg-transparent" />
-                <button onClick={addComment} type="button" className="bg-sky-500 p-2 rounded text-slate-50">Comment</button>
+                <input type="text" onChange={(e) => setCommentInput(e.target.value)} placeholder={loggedInUser? `Comment as ${loggedInUser?.displayName}` : " Input your comment"} value={commentInput} className="w-full outline-none bg-transparent" />
+                <button onClick={addComment} type="button" className="bg-sky-500 p-2 text-[12px] md:text-[15px] rounded text-slate-50">Comment</button>
                             </div>
                       </div>
                         </div>
